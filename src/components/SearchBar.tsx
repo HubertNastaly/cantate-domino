2c9541fa74bc41f60d31eca882bdf7d6b92c3b12
HubertNastaly <h.nastaly@gmail.com>
2023-04-27 14:49:03 +0200

    Style searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,6 @@
 import { lato } from "@/fonts"
 import { COLORS } from "@/utils/colors"
+import { IoIosSearch } from 'react-icons/io'
 import styled from "styled-components"
 
 interface Props {
@@ -9,24 +10,40 @@ interface Props {
 
 export const SearchBar = ({ value, onChange }: Props) => {
   return (
-    <Input
-      onChange={(event) => onChange(event.target.value)}
-      value={value}
-    />
+    <InputWrapper>
+      <IoIosSearch color={COLORS.accent} size={16} />
+      <Input
+        placeholder="Szukaj pieśni"
+        onChange={(event) => onChange(event.target.value)}
+        value={value}
+      />
+    </InputWrapper>
   )
 }
 
-const Input = styled.input`
+const InputWrapper = styled.div`
   width: 100%;
-  height: 48px;
+  height: 56px;
+  display: flex;
+  column-gap: 4px;
+  align-items: center;
   padding: 8px 16px;
   box-sizing: border-box;
   border-radius: 4px;
   border: 1px solid #cccccc;
-  font-size: 18px;
-  font-family: ${lato.style.fontFamily};
 
   &:focus {
     border-color: ${COLORS.primary};
+  }
+`
+
+const Input = styled.input`
+  flex: 1;
+  font-size: 18px;
+  font-family: ${lato.style.fontFamily};
+  border: none;
+
+  &:focus {
+    outline: none;
   }
 `
