593ea9f6686f87e013cd20e273515d21773272ee
HubertNastaly <h.nastaly@gmail.com>
2023-05-04 16:22:08 +0200

    Round time instead of taking ceiling

--- src/utils/formatTime.ts
+++ src/utils/formatTime.ts
@@ -4,7 +4,7 @@ export function formatTime(time: number | undefined) {
   if(!time) {
     return '00:00'
   }
-  const roundedTime = Math.ceil(time)
+  const roundedTime = Math.round(time)
   const minutes = Math.floor(roundedTime / SECONDS_IN_MINUTE)
   const seconds = roundedTime % SECONDS_IN_MINUTE
   return minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0')
