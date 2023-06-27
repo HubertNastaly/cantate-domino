ad3d5208fb6de5e3ba194743dde1b75cefb0462f
HubertNastaly <56291339+HubertNastaly@users.noreply.github.com>
2023-06-27 08:49:08 +0200

    Support keyboard navigation in SongAutocomplete (#34)

--- src/components/common/SongAutocomplete.tsx
+++ src/components/common/SongAutocomplete.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react"
+import { useRef, useState, KeyboardEvent, useCallback, HTMLProps, PropsWithChildren, useEffect, RefObject } from "react"
 import { SearchBar } from "./SearchBar"
 import { useOutsideClick, useSongs } from "@/hooks"
 import styled from "styled-components"
@@ -15,6 +15,7 @@ interface Props {
 export const Autocomplete = ({ onSelect }: Props) => {
   const [searchPhrase, setSearchPhrase] = useState('')
   const [isFocused, setIsFocused] = useState(false)
+  const [focusedListItemIndex, setFocusedListItemIndex] = useState<number>()
 
   const suggestionsRef = useRef<HTMLUListElement>(null)
   useOutsideClick(suggestionsRef, () => setIsFocused(false))
@@ -23,13 +24,37 @@ export const Autocomplete = ({ onSelect }: Props) => {
   const suggestions = data?.pages[0].songs
   const shouldShowSuggestions = searchPhrase.length > 0
 
+  // listItemIndex and suggestions as arguments and not dependencies in order not to redeclare function
+  const handleKeyDown = useCallback((event: KeyboardEvent<HTMLDivElement>, focusedListItemIndex: number | undefined, suggestions: Song[] | undefined) => {
+    switch(event.code) {
+      case 'ArrowDown': {
+        event.preventDefault()
+        return setFocusedListItemIndex(focusedListItemIndex =>
+          focusedListItemIndex === undefined ? 0 : Math.min(focusedListItemIndex + 1, SUGGESTIONS_COUNT - 1)
+        )
+      }
+      case 'ArrowUp': {
+        event.preventDefault()
+        return setFocusedListItemIndex(focusedListItemIndex =>
+          focusedListItemIndex === undefined ? undefined : Math.max(0, focusedListItemIndex - 1)
+        )
+      }
+      case 'Enter': {
+        if(suggestions && focusedListItemIndex !== undefined) {
+          onSelect(suggestions[focusedListItemIndex])
+        }
+        return
+      }
+    }
+  }, [setFocusedListItemIndex, onSelect])
+
   return (
-    <Container>
+    <Container onKeyDown={event => handleKeyDown(event, focusedListItemIndex, suggestions)}>
       <SearchBar value={searchPhrase} onChange={setSearchPhrase} onFocus={() => setIsFocused(true)} />
       {isFocused && (
         <Suggestions ref={suggestionsRef}>
-          {shouldShowSuggestions && suggestions?.map(song => (
-            <ListItem key={song.id} onClick={() => onSelect(song)}>
+          {shouldShowSuggestions && suggestions?.map((song, index) => (
+            <ListItem key={song.id} onClick={() => onSelect(song)} focused={index === focusedListItemIndex}>
               <SongCardStyled
                 song={song}
                 titlePlacement="right"
@@ -58,9 +83,10 @@ const Suggestions = styled.ul`
   ${smallShadow}
 `
 
-const ListItem = styled.li`
+const ListItem = styled.li<{ focused?: boolean }>`
   padding: 8px 16px;
   cursor: pointer;
+  background: ${props => props.focused ? COLORS.faded : COLORS.background};
 
   transition: background-color 0.2s ease-out;
 
@@ -70,7 +96,7 @@ const ListItem = styled.li`
   }
 
   &:hover {
-    background: #EEEEEE;
+    background: ${COLORS.faded};
   }
 `
 
